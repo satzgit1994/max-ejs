db29abf97685558821d2f10cd544428f5fc8e2ae
sathish V <sathish15767@poorvika.com>
2023-02-15 15:44:08 +0530

    added product array and pushing incoming data to it

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const product = []
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -14,6 +16,7 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  product.push({tilte: req.body.tilte})
   res.redirect('/');
 });
 
