e7a3f2d9b00993b47761877ed78a89db7f11e85c
sathish V <sathish15767@poorvika.com>
2023-02-15 16:05:22 +0530

    imported and added product data from add-product page

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,12 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(`${adminData}`)
+
 });
 
 module.exports = router;
